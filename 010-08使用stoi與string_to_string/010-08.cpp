9f3b4719795ca8e835e36dbc2eaa7496bc661358
108368085 <a0920528608@gmail.com>
2023-01-19 23:32:07 +0800

    Main

--- 010-08使用stoi與string_to_string/010-08.cpp
+++ 010-08使用stoi與string_to_string/010-08.cpp
@@ -7,7 +7,6 @@ using namespace std;
 int main() {
 
 	// int stoi (const string&  str, size_t* idx = 0, int base = 10);   PS:字串轉整數
-	// string to_string (int val);   									PS:整數轉字串
 	
 	string s1 = "111aa222ab333";
 	size_t st = 0;
@@ -18,12 +17,13 @@ int main() {
  	
 
 	
-	/* 
+	// string to_string (int val);   PS:整數轉字串
+
 	string s2 = to_string(123);
-	cout<<s2<<endl;
-	string s3 = "" + 123;
+	cout<<s2<<endl;         // 123
+	string s3 = s2 + "@";   // 123@
 	cout<<s3<<endl;
- 	*/
+ 	
 
 	return 0;
 }
